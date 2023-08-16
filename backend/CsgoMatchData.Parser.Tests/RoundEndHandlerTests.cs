9e72f0d82daed749adf17a1ad29bc0f506f65d07
Rasmus Løje <rasmusloejee@gmail.com>
2023-08-16 08:46:11 +0200

    tests: provided unit tests for round start/end handlers

---
+++ backend/CsgoMatchData.Parser.Tests/RoundEndHandlerTests.cs
@@ -0,0 +1,24 @@
+﻿using MatchData.Parser.Handlers;
+using MatchData.Parser.Models.Actions;
+
+namespace MatchData.Parser.Tests;
+
+public class RoundEndHandlerTests
+{
+    [Fact]
+    public void Given_match_data_log_When_log_is_round_end_Then_parse_correctly_to_round_end_event()
+    {
+        // Arrange
+        var roundEndActionHandler = new RoundEndHandler();
+        var actionText = @"11/28/2021 - 21:02:14: World triggered ""Round_End""";
+        var expectedRoundEndTime = new DateTime(2021, 11, 28, 21, 02, 14);
+
+        // Act
+        var result = roundEndActionHandler.Parse(actionText);
+
+        // Assert
+        Assert.IsType<RoundEndEvent>(result);
+        var roundEndAction = (RoundEndEvent)result;
+        Assert.True(roundEndAction.RoundEnd == expectedRoundEndTime);
+    }
+}
---
+++ backend/CsgoMatchData.Parser.Tests/RoundStartHandlerTests.cs
@@ -0,0 +1,24 @@
+﻿using MatchData.Parser.Handlers;
+using MatchData.Parser.Models.Actions;
+
+namespace MatchData.Parser.Tests;
+
+public class RoundStartHandlerTests
+{
+    [Fact]
+    public void Given_match_data_log_When_log_is_round_start_Then_parse_correctly_to_round_start_event()
+    {
+        // Arrange
+        var roundStartHandler = new RoundStartHandler();
+        var actionText = @"11/28/2021 - 21:01:12: World triggered ""Round_Start""";
+        var expectedRoundStartTime = new DateTime(2021, 11, 28, 21, 1, 12);
+
+        // Act
+        var result = roundStartHandler.Parse(actionText);
+
+        // Assert
+        Assert.IsType<RoundStartEvent>(result);
+        var roundStartAction = (RoundStartEvent)result;
+        Assert.True(roundStartAction.RoundStart == expectedRoundStartTime);
+    }
+}
