e1790535af08218f847a46e16a65864210cc27ed
Rasmus Løje <rasmusloejee@gmail.com>
2023-08-16 16:17:17 +0200

    tests: player extractor & time extractor

---
+++ backend/CsgoMatchData.Parser.Tests/Extractors/PlayerExtractorTests.cs
@@ -0,0 +1,23 @@
+﻿using CsgoMatchData.Parser.Helpers;
+using CsgoMatchData.Parser.Models;
+
+namespace CsgoMatchData.Parser.Tests.Extractors;
+
+public class PlayerExtractorTests
+{
+    [Theory]
+    [InlineData("\"ZywOo<26><STEAM_1:1:76700232><CT>\"", "ZywOo", TeamType.CounterTerrorist)]
+    [InlineData("\"b1t<32><STEAM_1:0:143170874><TERRORIST>\"", "b1t", TeamType.Terrorist)]
+    public void Given_match_data_log_When_extracting_player_Then_parse_correctly(
+        string actionText, 
+        string expectedPlayerName, 
+        TeamType expectedTeamType)
+    {
+        // Arrange & Act
+        var player = PlayerExtractor.ParsePlayerFromActionText(actionText);
+        
+        // Assert
+        Assert.True(player.Name == expectedPlayerName);
+        Assert.True(player.TeamType == expectedTeamType);
+    }
+}
---
+++ backend/CsgoMatchData.Parser.Tests/Extractors/TimeExtractorTests.cs
@@ -0,0 +1,37 @@
+﻿using CsgoMatchData.Parser.Helpers;
+
+namespace CsgoMatchData.Parser.Tests.Extractors;
+
+public class TimeExtractorTests
+{
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void ParseTimeFromActionText(string actionText, DateTime expectedDateTime)
+    {
+        // Arrange & Act
+        var time = TimeExtractor.ParseTimestampFromActionText(actionText);
+        
+        // Assert
+        Assert.True(time == expectedDateTime);
+    }
+    
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[]
+            {
+                @"11/28/2021 - 20:41:12: ""b1t<32><STEAM_1:0:143170874><TERRORIST>"" purchased ""molotov""", 
+                new DateTime(2021, 11, 28, 20, 41, 12)
+            },
+            new object[]
+            {
+                @"11/28/2021 - 20:58:05: ""Kyojin<8><STEAM_1:1:22851120><CT>"" money change 1700-300 = $1400 (tracked) (purchase: weapon_hegrenade)", 
+                new DateTime(2021, 11, 28, 20, 58, 5)
+            },
+            new object[]
+            {
+                @"11/28/2021 - 21:22:07: ""ZywOo<26><STEAM_1:1:76700232><TERRORIST>"" picked up ""c4""", 
+                new DateTime(2021, 11, 28, 21, 22, 7)
+            }
+        };
+}
